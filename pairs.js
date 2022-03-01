af26942a0e3b00633ce933c93ee3441b789fdf8e
Bipasha Agarwal <agarwalbipasha@gmail.com>
2022-03-01 10:59:51 +0530

    Solved and tested the 4th problem

---
+++ pairs.js
@@ -0,0 +1,4 @@
+function pairs(obj) {
+    return Object.entries(obj)
+}
+module.exports = { pairs };
---
+++ test/testPairs.js
@@ -0,0 +1,4 @@
+const pair = require("../pairs");
+
+const testObject = { name: 'Bruce Wayne', age: 36, location: 'Gotham' }; 
+console.log(pair.pairs(testObject));
