fe17dcc35197b023acc1362c75d1e419bc05d1ab
Bipasha Agarwal <agarwalbipasha@gmail.com>
2022-03-03 08:31:41 +0530

    Completed and tested function defaults

--- defaults.js
+++ defaults.js
@@ -1,10 +1,10 @@
 function defaults(obj, defaultProps) {
     return Object.keys(obj).reduce((result, key) => {
-        // console.log(result);
+        for (let key in defaultProps) {
         if (!obj.hasOwnProperty(key)) {
             result[key] = defaultProps[key];
             return result;
         }
-    }, {});
+    }}, {...obj});
 }
 module.exports = { defaults };
